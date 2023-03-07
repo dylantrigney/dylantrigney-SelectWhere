8767e18611139691fa73f0c0b36c8d66c664b3d8
dylantrigney <dylan.rigney@revature.net>
2023-03-07 18:01:35 +0000

    select where

--- src/main/java/FilterRecords.java
+++ src/main/java/FilterRecords.java
@@ -90,7 +90,7 @@ public class FilterRecords {
     /**
      * Problem 3: Given the employee table, write a query in the problem2.sql file to retrieve all the records
      * from the employee table that have a salary greater than $75000
-     *
+     *  
      *   employee table
      *   |  id  |   first_name   |   last_name   |  salary  |
      *   --------------------------------------------------
